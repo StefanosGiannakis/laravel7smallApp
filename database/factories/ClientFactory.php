9805b357c0b2710273714a601a07e443531c6291
root <satchfast@gmail.com>
2020-03-16 16:56:56 +0200

    seeding db and models relation

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Client;
+use Faker\Generator as Faker;
+ 
+$factory->define(Client::class, function (Faker $faker) {
+    $fakeDateTime=$faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now', $timezone = null);
+    return [
+        'name' => $faker->firstName,
+        'surname' => $faker->lastName,
+        'created_at' => $fakeDateTime,
+        'updated_at' => $fakeDateTime
+    ];
+});
